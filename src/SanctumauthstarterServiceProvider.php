7c85971f0a3429143f443483087cc041efc06dc6
ikechukwu kalu <ikechukwukalu@ikechukwus-MacBook-Pro.local>
2022-10-01 05:42:45 +0100

    Updated Package

--- src/SanctumauthstarterServiceProvider.php
+++ src/SanctumauthstarterServiceProvider.php
@@ -13,12 +13,12 @@ class SanctumauthstarterServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/api.php')->prefix('api');
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/migrations');
-        $this->loadViewsFrom(__DIR__.'/views', 'sanctumauthstarter');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php')->prefix('api');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/../migrations');
+        $this->loadViewsFrom(__DIR__.'/../views', 'sanctumauthstarter');
         $this->publishes([
-            __DIR__.'/views' => base_path('resources/views/ikechukwukalu/sanctumauthstarter'),
+            __DIR__.'/../views' => base_path('resources/views/ikechukwukalu/sanctumauthstarter'),
         ], 'views');
     }
 
